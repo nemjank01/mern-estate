cf4eed10ba9695e844ce0c68245a0992074d8b6e
nemjank01 <nemanja.jankovic2001@gmail.com>
2026-02-05 12:46:08 +0100

    add profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,44 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 cursor-pointer object-cover self-center my-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border border-slate-500 p-3 rounded-lg "
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="border border-slate-500 p-3 rounded-lg "
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border border-slate-500 p-3 rounded-lg "
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 transition-all disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between items-center mt-4">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
