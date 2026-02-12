cb8fb730adc4ea365b42430e54d2e8a2dc3f0ae7
nemjank01 <nemanja.jankovic2001@gmail.com>
2026-02-12 16:18:21 +0100

    add delete listing api route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -1,10 +1,27 @@
 import Listing from "../models/listingModel.js";
+import { errorHandler } from "../utils/error.js";
 
 export async function createListing(req, res, next) {
   try {
     const listing = await Listing.create(req.body);
 
     res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function deleteListing(req, res, next) {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) return next(errorHandler(404, "Listing not found!"));
+
+  if (req.user.id !== listing.userRef.toString())
+    return next(errorHandler(404, "You can only delete your own listings!"));
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Listing has been deleted!" });
   } catch (error) {
     next(error);
   }
