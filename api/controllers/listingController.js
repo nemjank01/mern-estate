ec4631508e36d3f7fd813d5ac6dd7c8989b014d3
nemjank01 <nemanja.jankovic2001@gmail.com>
2026-02-12 16:39:03 +0100

    add update listing api route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -26,3 +26,24 @@ export async function deleteListing(req, res, next) {
     next(error);
   }
 }
+
+export async function updateListing(req, res, next) {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) return next(errorHandler(404, "Listing not found!"));
+
+  if (req.user.id !== listing.userRef.toString())
+    return next(errorHandler(404, "You can only update your own listings!"));
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+}
