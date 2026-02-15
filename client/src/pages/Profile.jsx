71c9c92b59e201a16e748e530bf2c6018bd07431
nemjank01 <nemanja.jankovic2001@gmail.com>
2026-02-15 11:27:39 +0100

    add getListing api route and complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -237,7 +237,9 @@ export default function Profile() {
                 >
                   Delete
                 </button>
-                <button className="text-green-700 uppercase">Edit</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
