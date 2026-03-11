5182558f8e71f81fb723ab997a8633b9b37f5e53
nemjank01 <nemanja.jankovic2001@gmail.com>
2026-03-11 10:10:12 +0100

    add about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,39 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 mx-auto max-w-6xl">
+      <h1 className="text-3xl text-slate-800 font-bold mb-4">
+        About Real Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+        ratione, harum delectus non odit error nulla fugit tempore eligendi,
+        inventore, exercitationem repudiandae! Ullam beatae veritatis
+        exercitationem, iste quis veniam animi. ratione, harum delectus non odit
+        error nulla fugit tempore eligendi, inventore, exercitationem
+        repudiandae! Ullam beatae veritatis exercitationem, iste quis veniam
+        animi.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus,
+        sunt quo iusto necessitatibus et nam ut quod cupiditate, voluptatum cum
+        dolor delectus saepe amet iure magni sit ullam ratione! At! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Doloribus, sunt quo iusto
+        necessitatibus et nam ut quod cupiditate, voluptatum cum dolor delectus
+        saepe amet iure magni sit ullam ratione! At! Lorem ipsum dolor sit, amet
+        consectetur adipisicing elit. Doloribus, sunt quo iusto necessitatibus
+        et nam ut quod cupiditate, voluptatum cum dolor delectus saepe amet iure
+        magni sit ullam ratione! At!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ipsa quae
+        esse deserunt reprehenderit placeat numquam debitis dolore sunt eius
+        excepturi itaque suscipit repudiandae natus, sit commodi! Distinctio,
+        illo consequatur. esse deserunt reprehenderit placeat numquam debitis
+        dolore sunt eius excepturi itaque suscipit repudiandae natus, sit
+        commodi! Distinctio, illo consequatur.
+      </p>
+    </div>
+  );
 }
